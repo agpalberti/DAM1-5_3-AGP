4b80b72e76de0d5118b5c7ceca656f2e4e213854
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-12-21 00:34:01 +0100

    Terminado y comentado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ class Agenda() {
 
     fun delete(contacto: Contacto): Boolean = listaDeContactos.remove(contacto)
 
-    fun listado(): List<Contacto> = listaDeContactos.sortedBy { it.nombre }
+    fun listado(): List<Contacto> = listaDeContactos.sortedBy { it.nombre } //Al ordenar Strings, utiliza el orden alfabético.
 
     fun filtra(texto: String): List<Contacto> = listaDeContactos.filter { texto.uppercase() in it.nombre.uppercase() }
 
@@ -19,6 +19,7 @@ class Agenda() {
 }
 
 class Contacto(nombre: String) {
+    //Compruebo en los setters de número y nombre que los formatos se cumplan.
     var nombre: String = nombre
         set(value) {
             if (value.isNotEmpty() && value.first().isLetter()) field = value
@@ -55,16 +56,21 @@ fun main() {
 
     while (!salir) {
         input = readLine() ?: ""
-        val command = input.split(" ")
+        val command = input.split(" ") //Para separar los inputs de los comandos con sus opciones, como el filtro y texto_a_buscar.
 
+        //Me aseguro que la entrada no esté vacía para evitar problemas.
         if (input.isNotEmpty()) {
+            //Compruebo primero si el primer carácter es un número o un '+'.
             if (input[0].isDigit() || input[0] == '+') {
                 val consulta = agenda.consultaNumero(input)
+                //Si se encuentra en la agenda, se imprime la información.
                 if (consulta != null) {
                     println("\nEste número pertenece a ${consulta.nombre}.")
                 } else {
+                    //Si no, te pide que introduzcas la información para agregar al contacto.
                     println("\nContacto no encontrado. Introduzca el nombre para registrarlo.")
                     try {
+                        //Compruebo que se cumpla las condiciones del nombre y el número mediante el setter de la clase Contacto. En caso de que no se cumpla, saltará una excepción. Por eso el try/catch.
                         val nombre = readLine() ?: ""
                         agenda.add(Contacto(nombre, input))
                         println("\nContacto agregado de forma correcta.")
@@ -72,8 +78,10 @@ fun main() {
                         println("\nSe ha introducido algún dato de forma incorrecta. Operación cancelada.")
                     }
                 }
+            //Si no es un número, será un comando o un nombre. Compruebo que no esté en la lista de comandos permitidos.
             } else if (command.first() !in listaComandos) {
                 val consulta = agenda.consultaNombre(input)
+                //Se aplica lo mismo que para el número con el nombre.
                 if (consulta != null) {
                     println("\nEl número de ${consulta.nombre} es ${consulta.numero}.")
                 } else {
@@ -86,8 +94,9 @@ fun main() {
                         println("\nSe ha introducido algún dato de forma incorrecta. Operación cancelada.")
                     }
                 }
+            //Si está en la lista de comandos, será una de las siguientes opciones.
             } else when (command.first()) {
-                "adios" -> salir = true
+                "adios" -> salir = true //Para salir del bucle y cerrar el programa.
                 "listado" -> {
                     val listaContactos = agenda.listado()
                     if (listaContactos.isNotEmpty()) {
@@ -96,6 +105,7 @@ fun main() {
                     } else println("\nNo hay contactos registrados.")
                 }
                 "filtra" -> {
+                    //Compruebo primero que se haya introducido un filtro para el comando. Para eso, el split tendrá que haber dividido el String en 2 o más Strings.
                     if (command.size > 1) {
                         val filtro = command[1]
                         val agendaFiltrada = agenda.filtra(filtro)
@@ -113,7 +123,6 @@ fun main() {
                     )
                 }
             }
-        } else println("\nNo has introducido nada.")
-
+        } else println("\nNo has introducido nada.") //Si no se ha introducido nada, se avisa al usuario.
     }
 }
